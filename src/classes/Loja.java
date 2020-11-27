4b4184a8cd52eda62ab35e7d2cd8119536c6de43
jacksrm <62717829+jacksrm@users.noreply.github.com>
2020-11-27 19:38:54 -0300

    alterações

---
+++ src/classes/Loja.java
@@ -0,0 +1,17 @@
+package classes;
+
+import java.util.ArrayList;
+
+public class Loja {
+  private ArrayList<Computador> computers;
+
+  
+
+  public ArrayList<Computador> getComputers() {
+    return this.computers;
+  }
+
+  public void setComputers(ArrayList<Computador> computers) {
+    this.computers = computers;
+  }
+}
--- src/classes/Notebook.java
+++ src/classes/Notebook.java
@@ -1,51 +1,23 @@
 package classes;
 
-public  abstract class Notebook {
-	 private String nameCPU;
-	  private Integer sizeRAM;
-	  private Double price;
-	  private Boolean hasWebCam ;
-	  
-	  public Notebook(String nameCPU, Integer sizeRAM, Double price, Boolean hasWebCam) {
-		    this.nameCPU = nameCPU;
-		    this.sizeRAM = sizeRAM;
-		    this.price = price;
-		    this.hasWebCam = hasWebCam;
-		  }
+public abstract class Notebook extends Computador {
+  private Boolean hasWebCam;
 
-		  public abstract Double calcDiscount();
+  public Notebook(String nameCPU, Integer sizeRAM, Double price, Boolean hasWebCam) {
+    super(nameCPU, sizeRAM, price);
+    this.hasWebCam = hasWebCam;
+  }
 
-		  public String getNameCPU() {
-		    return this.nameCPU;
-		  }
+  public Double calcDiscount() {
+    
+  };
 
-		  public void setNameCPU(String nameCPU) {
-		    this.nameCPU = nameCPU;
-		  }
 
-		  public Integer getSizeRAM() {
-		    return this.sizeRAM;
-		  }
-		  
-		  public Boolean gethasWebCam(Boolean hasWebCam) {
-			    return this.hasWebCam;
-			  }
-		  
-		  public void sethasWebCam(Boolean hasWebCam) {
-			    this.hasWebCam = hasWebCam;
-			  }
+  public Boolean gethasWebCam(Boolean hasWebCam) {
+    return this.hasWebCam;
+  }
 
-		  public void setSizeRAM(Integer sizeRAM) {
-		    this.sizeRAM = sizeRAM;
-		  }
-
-		  public Double getPrice() {
-		    return this.price;
-		  }
-
-		  public void setPrice(Double price) {
-		    this.price = price;
-		  }
-		
-
-		}
+  public void sethasWebCam(Boolean hasWebCam) {
+    this.hasWebCam = hasWebCam;
+  }
+}
