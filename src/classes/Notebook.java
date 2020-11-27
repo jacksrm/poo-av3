b83474b39bfecbc9fb6fe3184d6fa1045b8b8d2a
Mayh6m <70498107+Mayh6m@users.noreply.github.com>
2020-11-27 19:36:32 -0300

    Notebook full

--- src/classes/Notebook.java
+++ src/classes/Notebook.java
@@ -1,5 +1,51 @@
 package classes;
 
-public class Notebook {
+public  abstract class Notebook {
+	 private String nameCPU;
+	  private Integer sizeRAM;
+	  private Double price;
+	  private Boolean hasWebCam ;
+	  
+	  public Notebook(String nameCPU, Integer sizeRAM, Double price, Boolean hasWebCam) {
+		    this.nameCPU = nameCPU;
+		    this.sizeRAM = sizeRAM;
+		    this.price = price;
+		    this.hasWebCam = hasWebCam;
+		  }
 
-}
+		  public abstract Double calcDiscount();
+
+		  public String getNameCPU() {
+		    return this.nameCPU;
+		  }
+
+		  public void setNameCPU(String nameCPU) {
+		    this.nameCPU = nameCPU;
+		  }
+
+		  public Integer getSizeRAM() {
+		    return this.sizeRAM;
+		  }
+		  
+		  public Boolean gethasWebCam(Boolean hasWebCam) {
+			    return this.hasWebCam;
+			  }
+		  
+		  public void sethasWebCam(Boolean hasWebCam) {
+			    this.hasWebCam = hasWebCam;
+			  }
+
+		  public void setSizeRAM(Integer sizeRAM) {
+		    this.sizeRAM = sizeRAM;
+		  }
+
+		  public Double getPrice() {
+		    return this.price;
+		  }
+
+		  public void setPrice(Double price) {
+		    this.price = price;
+		  }
+		
+
+		}
