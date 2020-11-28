c8d877a67b57a4a2e73f2a1c772c306406867bfd
jacksrm <62717829+jacksrm@users.noreply.github.com>
2020-11-27 21:19:23 -0300

    av finalizada

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -17,16 +19,16 @@ public class App {
       File fileNotebooks = new File("src/files/notebooks.txt");
       Scanner deskScanner = new Scanner(fileDesktops);
       Scanner noteScanner = new Scanner(fileNotebooks);
-
+      
       while (deskScanner.hasNext()) {
         String cpuName = deskScanner.next();
         Integer ramSize = deskScanner.nextInt();
         Double price = deskScanner.nextDouble();
         Boolean allInOne = deskScanner.nextBoolean();
-
+        
         computers.add(new Desktop(cpuName, ramSize, price, allInOne));
       }
-
+      
       while (noteScanner.hasNext()) {
         String cpuName = noteScanner.next();
         Integer ramSize = noteScanner.nextInt();
@@ -35,29 +37,52 @@ public class App {
 
         computers.add(new Notebook(cpuName, ramSize, price, hasWebCam));
       }
-
+      
       deskScanner.close();
       noteScanner.close();
-
-      // for (int i = 0; i < computers.size(); i++) {
-      //   if(computers.get(i) instanceof Desktop) {
-      //     Desktop auxDesk = (Desktop)computers.get(i);
-      //     auxDesk.print();
-      //     System.out.println();
-      //   }
-      //   if(computers.get(i) instanceof Notebook) {
-      //     Notebook auxNote = (Notebook)computers.get(i);
-      //     auxNote.print();
-      //     System.out.println();
-      //   }
-      // }
 
     } catch (FileNotFoundException e) {
       System.out.println("Arquivo não encontrado!");
     }
     
-
+    File fileWebcamAllinOne = new File("src/files/webcamAllinOne.txt");
+    
     Loja loja = new Loja(computers);
 
+    try {
+      if (fileWebcamAllinOne.createNewFile()) 
+        System.out.println("Criado!");
+      else 
+        System.out.println("Erro ao criar");
+
+      FileWriter writer = new FileWriter(fileWebcamAllinOne);
+      
+      for (int i = 0; i < loja.webcamAllinOne().size(); i++) {
+        if (loja.webcamAllinOne().get(i) instanceof Desktop) {
+          Desktop desk = (Desktop)loja.webcamAllinOne().get(i);
+          writer.write(
+            desk.getNameCPU() + " " +
+            desk.getSizeRAM() + " " +
+            desk.getPrice() + " " +
+            desk.getIsAllInOne() + " " +
+            desk.calcDiscount() + "\n"
+          );
+        }
+
+        if (loja.webcamAllinOne().get(i) instanceof Notebook) {
+          Notebook note = (Notebook)loja.webcamAllinOne().get(i);
+          writer.write(
+            note.getNameCPU() + " " +
+            note.getSizeRAM() + " " +
+            note.getPrice() + " " +
+            note.getHasWebCam() + " " +
+            note.calcDiscount() + "\n" 
+          );
+        }
+      }
+      writer.close();
+    } catch (IOException e) {
+      System.out.println("Um erro ocorreu!");
+    }
   }
 }
